b13223d7f3b9856b47c399d7421da1ea890d8209
Pete Boutle <Peter.Boutle@gmail.com>
2018-11-09 19:42:55 +0000

    Login page added

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello World"
+    user = {'username': 'Pete'}
+    return render_template('index.html', title='Home',user=user)
+    
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In',form = form)
