e8dc30ebeed7cd98b35dbd0a45663672bc3dc276
Pete Boutle <Peter.Boutle@gmail.com>
2018-11-09 22:32:44 +0000

    added database

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+
+class User(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    classname = db.Column(db.String(64), index=True, unique=False)
+    role = db.Column(db.String(64))
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
